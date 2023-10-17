9cc6cb68105bc49ea64bf20c4989482a47fde2aa
Arunjith Surendran <arunjithsurendran22@gmail.com>
2023-10-17 09:27:57 +0530

    new changes

--- app.js
+++ app.js
@@ -13,10 +13,8 @@ app.use(expressLayouts);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-
 //Routes
-app.use('/',require('./server/routes/index'))
-
+app.use("/", require("./server/routes/index"));
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT${PORT}`);
